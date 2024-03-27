79fac99cfd2cebddce1e67f15bd3aa76eb3d1874
Pape Barka <papebarka@gmail.com>
2024-03-27 19:39:58 +0000

    Project starter

---
+++ lib/csvex.ex
@@ -0,0 +1,18 @@
+defmodule Csvex do
+  @moduledoc """
+  Documentation for `Csvex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Csvex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/csvex_test.exs
@@ -0,0 +1,8 @@
+defmodule CsvexTest do
+  use ExUnit.Case
+  doctest Csvex
+
+  test "greets the world" do
+    assert Csvex.hello() == :world
+  end
+end
